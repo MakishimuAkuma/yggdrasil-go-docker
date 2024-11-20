038a4c39b0cad62d280092966437e8d6f6107d99
MakishimuAkuma <61804392+MakishimuAkuma@users.noreply.github.com>
2024-11-21 01:13:38 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ CONF_DIR="/etc/yggdrasil-network"
 
 if [ ! -f "$CONF_DIR/config.conf" ]; then
   echo "generate $CONF_DIR/config.conf"
-  yggdrasil --genconf > "$CONF_DIR/config.conf"
+  ./yggdrasil --genconf > "$CONF_DIR/config.conf"
 fi
 
-yggdrasil --useconf < "$CONF_DIR/config.conf"
+./yggdrasil --useconf < "$CONF_DIR/config.conf"
 
 exit $?
